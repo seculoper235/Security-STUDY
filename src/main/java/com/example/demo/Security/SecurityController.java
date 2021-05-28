0a4d5cc4fdb82689c83ea2fc5691778eb58e7253
seculoper <mir8725@naver.com>
2021-05-28 23:36:24 +0900

    별도의 login 인증 처리: Controller에 인증 처리 API 추가

--- src/main/java/com/example/demo/Security/SecurityController.java
+++ src/main/java/com/example/demo/Security/SecurityController.java
@@ -1,15 +1,28 @@
 package com.example.demo.Security;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.example.demo.Domain.Dto.PeopleRequest;
+import com.example.demo.Domain.Dto.PeopleResponse;
+import com.example.demo.Service.SecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SecurityController {
+    private final SecurityService securityService;
+
     @GetMapping("/")
     public String home() {
         return ("<h2>Welcome!</h2><a href=\"/login\"> 로그인하기 </a>");
+    }
+
+    @PostMapping("/loginProc")
+    public ResponseEntity<PeopleResponse> loginProc(@RequestBody PeopleRequest peopleRequest) {
+        return ResponseEntity.ok(securityService.signUp(peopleRequest));
     }
 
     @GetMapping("/users")

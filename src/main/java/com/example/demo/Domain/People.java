7c74a6dbf5440a7d0e9e4774e71b48580f05493d
seculoper <mir8725@naver.com>
2021-05-29 23:17:48 +0900

    JPA: People 엔티티 수정 및 MyAuthority 엔티티 생성

--- src/main/java/com/example/demo/Domain/People.java
+++ src/main/java/com/example/demo/Domain/People.java
@@ -1,43 +1,39 @@
 package com.example.demo.Domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.example.demo.Security.MyAuthority;
+import lombok.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.SpringSecurityCoreVersion;
 import org.springframework.security.core.userdetails.User;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 
 // DB 데이터를 받는 객체
-@Getter @Setter
+@Entity
+@Getter
 @NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "people")
+@EqualsAndHashCode(of = "username")
 public class People {
     /* 시큐리티 속성 */
     private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
 
     private static final Log logger = LogFactory.getLog(User.class);
 
+    @Id
+    private String id;
+
     private String password;
 
-    private String username;
-
-    private Set<GrantedAuthority> authorities;
-
-    private boolean accountNonExpired;
-
-    private boolean accountNonLocked;
-
-    private boolean credentialsNonExpired;
-
-    private boolean enabled;
-
+    @OneToMany(mappedBy = "people")
+    private Set<MyAuthority> authorities;
 
     /* 일반 DB 속성 */
-    private String nickname;
+    private String username;
 }
---
+++ src/main/java/com/example/demo/Security/MyAuthority.java
@@ -0,0 +1,22 @@
+package com.example.demo.Security;
+
+import com.example.demo.Domain.People;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "myauthority")
+@EqualsAndHashCode(of = "id")
+public class MyAuthority {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+
+    @ManyToOne
+    @JoinColumn(name = "people_id")
+    private People people;
+}

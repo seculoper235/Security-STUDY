2531aab16fb4d1af8dbbd01583a4c6c0dad8d325
seculoper <mir8725@naver.com>
2021-05-31 22:26:51 +0900

    PeopleRepository 생성

--- src/main/java/com/example/demo/Repository/PeopleRepository.java
+++ src/main/java/com/example/demo/Repository/PeopleRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.Repository;
 
-public class PeopleRepository {
+import com.example.demo.Domain.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeopleRepository extends JpaRepository<People, String> {
 }

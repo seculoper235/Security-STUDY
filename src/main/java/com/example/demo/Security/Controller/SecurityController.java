6aff95d7ab454997ca6a7d68418d1a63de18d6ce
seculoper <mir8725@naver.com>
2021-06-08 14:27:37 +0900

    간단한 결과 URL 작성

--- src/main/java/com/example/demo/Security/Controller/SecurityController.java
+++ src/main/java/com/example/demo/Security/Controller/SecurityController.java
@@ -1,23 +1,17 @@
 package com.example.demo.Security.Controller;
 
-import com.example.demo.Domain.Dto.PeopleRequest;
-import com.example.demo.Domain.Dto.PeopleResponse;
 import com.example.demo.Security.Service.OauthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/auth", consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/")
 public class SecurityController {
     private final OauthService oauthService;
 
-    @GetMapping
-    public String home() {
-        return "<h2>OAuth 인증에 성공하였습니다~!</h2>";
+    @GetMapping("/google")
+    public String googleHome() {
+        return "<h2>Google 인증에 성공하였습니다~!</h2>";
     }
 }

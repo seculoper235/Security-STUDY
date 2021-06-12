ebc47089465e796eb42ae2dafbdee075c8c8ae71
seculoper <mir8725@naver.com>
2021-06-12 23:58:57 +0900

    각 Endpoint에 대한 설명

--- src/main/java/com/example/demo/Security/Controller/SecurityController.java
+++ src/main/java/com/example/demo/Security/Controller/SecurityController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.*;
 public class SecurityController {
     private final OauthService oauthService;
 
-    @GetMapping("/google")
+    @GetMapping("/login/oauth2/callback/google")
     public String googleHome() {
         return "<h2>Google 인증에 성공하였습니다~!</h2>";
+    }
+
+    @GetMapping("/unauth")
+    public String notAuth() {
+        return "<h2>Google 로그아웃 되었습니다~!</h2>";
     }
 }

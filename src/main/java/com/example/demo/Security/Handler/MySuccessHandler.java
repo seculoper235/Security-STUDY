ffa01c4ee74658602c8a933b7c5b5c1e295e4e11
seculoper <mir8725@naver.com>
2021-06-19 13:35:59 +0900

    SuccessHandler 수정

--- src/main/java/com/example/demo/Security/Handler/MySuccessHandler.java
+++ src/main/java/com/example/demo/Security/Handler/MySuccessHandler.java
@@ -1,6 +1,9 @@
 package com.example.demo.Security.Handler;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +12,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public class MySuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-        if(request.isUserInRole("ROLE_ADMIN")) {
+        if(authentication.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"))) {
             response.sendRedirect("/admin");
         }
         else {
-            response.sendRedirect("/loginSuccess");
+            response.sendRedirect("/users");
         }
 
         super.onAuthenticationSuccess(request, response, authentication);
